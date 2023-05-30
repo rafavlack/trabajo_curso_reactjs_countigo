7ae6121d85951d8630bb88f04a6e6d8d97ff82dd
rbarrientos@soaint.com <rbarrientos@soaint.com>
2023-05-30 12:23:38 -0400

    update deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base: "/trabajo_curso_reactjs_countigo/",
 })
